4e09272a313afa3d2bbad9e20acbbcdff1eb22e6
Fidi <136447325+Fidiiii@users.noreply.github.com>
2023-06-13 13:55:47 +0000

    update title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,7 +6,7 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar"  class="title is-2" style="color:mediumspringgreen">
+        <b-navbar-item id="fontnavbar"  class="title is-2" style="color:aquamarine">
           Fidigram
         </b-navbar-item>
       </template>
